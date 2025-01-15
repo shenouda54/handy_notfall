bc5ffce11ddedf5c0dc633472a7e6ae15f3f1304
pc <@https://github.com/shenouda54/islami.git>
2025-01-15 23:18:20 +0200

    start firebase 1.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:handy_notfall/firebase_options.dart';
 import 'package:handy_notfall/screens/splash_screen.dart';
 
-void main(){
+void main()async{
+WidgetsFlutterBinding.ensureInitialized(); // لو مش موجود السطر دا هيجري علي run app علطول
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
